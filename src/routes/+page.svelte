ec053abefa2b2714e3e782da25ca86419670633f
john221wick <bhushanbharat6958@gmail.com>
2025-06-01 18:22:54 +0530

    removed blogs page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
-<script>
-	import Frontpage from '$lib/components/Frontpage.svelte';
-</script>
+<div class="flex items-center justify-center p-4 font-sans md:p-8">
+	<main class="mx-auto w-full max-w-2xl">
+		<section class="w-full space-y-6">
+			<p class="mb-4 text-lg md:text-xl">Hello everyone 👋</p>
 
-<Frontpage />
+			<h1 class="mb-6 text-4xl font-bold md:text-5xl">Bhushan</h1>
+
+			<div class="space-y-4 text-base leading-relaxed md:text-lg">
+				<p>
+					I'm a student pursuing Bachelor of Technology (4th Year) from Central University Haryana
+				</p>
+
+				<p>
+					Working with MLOps, AI Agents, Building LLM etc<br />
+					and exploring DevOps ecosystems
+				</p>
+				<p class="text-sm md:text-base">
+					Note: This portfolio is under development, some links are not available, I will be
+					updating it soon.
+				</p>
+			</div>
+		</section>
+	</main>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,123 +1,84 @@
 <script>
 	import { marked } from 'marked';
-	import Prism from 'prismjs';
-	import { onMount } from 'svelte';
-	import 'prismjs/themes/prism-tomorrow.css';
-	import 'prismjs/plugins/autoloader/prism-autoloader';
-
-	// For dynamically deciding the language
-	onMount(() => {
-		Prism.plugins.autoloader.languages_path =
-			'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/';
-	});
 
 	let projectsOngoing = [
 		{
-			date: '2025',
+			techStack: 'AI WebDev',
 			title: 'Agentic CI/CD',
 			href: 'https://github.com/bbl-sh/ke-automated-ci-cd'
 		},
 		{
-			date: '2025',
+			techStack: '2025',
 			title: 'Leetcode clone',
 			href: 'data/devops/bash-scripting.md'
 		},
 		{
-			date: '2025',
+			techStack: '2025',
 			title: 'Building JUDGE0 api via Docker',
 			href: 'https://github.com/bbl-sh/dg-projects'
 		},
 		{
-			date: '2024',
+			techStack: '2024',
 			title: 'Github actions and Terraform',
 			href: 'https://github.com/bbl-sh/actions-01'
 		},
 		{
-			date: '2024',
+			techStack: '2024',
 			title: 'Hugging Face Library',
 			href: 'https://github.com/bbl-sh/literate-invention/tree/main/hugging-face/nlp'
 		},
 		{
-			date: '2025',
+			techStack: '2025',
 			title: 'Building CodeCrafters Clone',
 			href: 'https://github.com/bbl-sh/ccraft'
 		},
 		{
-			date: 'will be updated',
+			techStack: 'will be uptechStackd',
 			title: 'Making miniGPT',
 			href: 'https://github.com/bbl-sh/making-minigpt'
 		},
 		{
-			date: '2024',
+			techStack: '2024',
 			title: 'Training miniGPT over Pytorch Distributed',
 			href: '/post/svelte5/simplicity.md'
 		},
 		{
-			date: '2024',
+			techStack: '2024',
 			title: 'Basics of Golang',
 			href: 'https://github.com/bbl-sh/golang'
 		},
 		{
-			date: '2023',
+			techStack: '2023',
 			title: 'Implementing RDT 3.0, tcp etc protocol in python',
 			href: 'https://github.com/bbl-sh/computer-networks'
 		},
 		{
-			date: '2022',
+			techStack: '2022',
 			title: 'Basics of pytorch',
 			href: 'https://github.com/bbl-sh/pytorch-basics'
 		}
 	];
-
-	let markdownContent = $state();
-
-	async function loadMarkdown(path) {
-		try {
-			const res = await fetch(path);
-			if (!res.ok) throw new Error('Failed to fetch');
-			const text = await res.text();
-			markdownContent = marked.parse(text);
-		} catch (err) {
-			console.error('Error loading Markdown:', err);
-			markdownContent = '<p class="text-red-500">Failed to load content.</p>';
-		}
-	}
-
-	// To ensure syntax highlighting after DOM update
-	$effect(() => {
-		if (markdownContent) {
-			requestAnimationFrame(() => {
-				const codeBlocks = document.querySelectorAll('pre code');
-				codeBlocks.forEach((block) => Prism.highlightElement(block));
-			});
-		}
-	});
 </script>
 
-{#if markdownContent}
-	<div class="text-white-800 max-w-2xl rounded-lg p-4 text-left leading-relaxed shadow-md sm:p-8">
-		<div class="prose prose-invert max-w-none">
-			{@html markdownContent}
-		</div>
+<section>
+	<h1 class="mb-8 text-2xl font-medium">Projects</h1>
+	<div>
+		{#each projectsOngoing as project, index (project.title)}
+			<a
+				href={project.href}
+				class="mb-5 flex flex-col space-y-1 transition-opacity duration-200 hover:opacity-80"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<div
+					class="flex w-full flex-col items-start justify-between space-y-1 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2"
+				>
+					<h2 class="text-black dark:text-white">{project.title}</h2>
+					<p class="text-neutral-600 dark:text-neutral-400">
+						{project.techStack}
+					</p>
+				</div>
+			</a>
+		{/each}
 	</div>
-{:else}
-	<div class="flex flex-col items-center p-3">
-		<ul class="w-full max-w-[600px] list-none p-0">
-			{#each projectsOngoing as { date, title, href } (title)}
-				<li class="mb-2 flex flex-wrap items-baseline">
-					<span
-						class="mr-2 min-w-[60px] flex-shrink-0 text-[10px] text-gray-500 sm:min-w-[80px] sm:text-xs md:min-w-[100px] md:text-sm"
-					>
-						{date}
-					</span>
-					<a
-						{href}
-						class="text-xs font-bold text-inherit no-underline hover:underline sm:text-sm md:text-base"
-					>
-						{title}
-					</a>
-				</li>
-			{/each}
-		</ul>
-	</div>
-{/if}
+</section>
